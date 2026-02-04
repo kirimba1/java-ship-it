f1549c492aa97d405915ccef82b2cac4628520ff
kirusha <kirillkomlyashov@yandex.ru>
2026-02-04 16:36:10 +0300

    refactor weight limit tests

--- test/ru/yandex/practicum/DeliveryBoxWeightLimitTest.java
+++ test/ru/yandex/practicum/DeliveryBoxWeightLimitTest.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum;
 
-
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.delivery.ParcelBox;
 import ru.yandex.practicum.delivery.StandardParcel;
@@ -8,26 +8,45 @@ import ru.yandex.practicum.delivery.StandardParcel;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DeliveryBoxWeightLimitTest {
+
+    private ParcelBox<StandardParcel> box;
+    private StandardParcel parcel5kg;
+    private StandardParcel anotherParcel5kg;
+    private StandardParcel parcel1kg;
+
+    @BeforeEach
+    void setUp() {
+        box = new ParcelBox<>(10);
+        parcel5kg = new StandardParcel(5, 1, "Посылка 5кг", "Адрес");
+        anotherParcel5kg = new StandardParcel(5, 1, "Посылка 5кг", "Адрес");
+        parcel1kg = new StandardParcel(1, 1, "Посылка 1кг", "Адрес");
+    }
+
     @Test
-    void testParcelBoxWeightLimits() {
-        ParcelBox<StandardParcel> box = new ParcelBox<>(10);
-        StandardParcel p1 = new StandardParcel(5, 1, "Посылка 1", "Адрес");
-        StandardParcel p2 = new StandardParcel(5, 1, "Посылка 2", "Адрес");
-        StandardParcel p3 = new StandardParcel(1, 1, "Лишняя", "Адрес");
+    void shouldAddParcelWhenSpaceAvailable() {
+        box.addParcel(parcel5kg);
 
-        // стандартный сценарий
-        box.addParcel(p1);
-        assertEquals(1, box.getAllParcels().size());
-        assertEquals(5, box.getCurrentWeight());
+        assertEquals(1, box.getAllParcels().size(), "В коробке должна быть 1 посылка");
+        assertEquals(5, box.getCurrentWeight(), "Вес коробки должен быть 5 кг");
+    }
 
-        // заполнение ровно до лимита
-        box.addParcel(p2);
-        assertEquals(2 , box.getAllParcels().size());
-        assertEquals(10, box.getCurrentWeight());
+    @Test
+    void shouldAddParcelWhenFillingExactlyToLimit() {
+        box.addParcel(parcel5kg);
+        box.addParcel(anotherParcel5kg);
 
-        // превышение лимита
-        box.addParcel(p3);
-        assertEquals(2, box.getAllParcels().size(), "Посылка не должна была добавиться в полную коробку");
-        assertEquals(10, box.getCurrentWeight());
+        assertEquals(2, box.getAllParcels().size(), "В коробке должно быть 2 посылки");
+        assertEquals(10, box.getCurrentWeight(), "Вес коробки должен быть равен лимиту (10 кг)");
+    }
+
+    @Test
+    void shouldNotAddParcelWhenLimitExceeded() {
+        box.addParcel(parcel5kg);
+        box.addParcel(anotherParcel5kg);
+
+        box.addParcel(parcel1kg);
+
+        assertEquals(2, box.getAllParcels().size(), "Лишняя посылка не должна была добавиться");
+        assertEquals(10, box.getCurrentWeight(), "Вес не должен превышать лимит");
     }
 }
