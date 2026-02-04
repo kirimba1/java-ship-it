fec6422c46936e84417aed3c745f3746edede7cc
kirusha <kirillkomlyashov@yandex.ru>
2026-02-04 13:55:08 +0300

    split DeliveryExpiredTest into granular boundary checks

--- test/ru/yandex/practicum/DeliveryExpiredTest.java
+++ test/ru/yandex/practicum/DeliveryExpiredTest.java
@@ -7,18 +7,25 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class DeliveryExpiredTest {
+
+    private final PerishableParcel parcel = new PerishableParcel(2, 5,
+            "Молоко", "Адрес", 3);
+
     @Test
-    void testIsExpired() {
-        // Отправлена в 5 день живет 3 дня. Годна до дня 8 включительно
-        PerishableParcel pp = new PerishableParcel(2, 5, "Молоко",
-                "Адрес", 3);
+    void shouldNotBeExpiredBeforeDeadline() {
+        assertFalse(parcel.isExpired(7),
+                "Посылка должна быть свежей на 7-й день");
+    }
 
-        assertFalse(pp.isExpired(6), "Должна быть годна на 6-й день");
+    @Test
+    void shouldNotBeExpiredOnDeadline() {
+        assertFalse(parcel.isExpired(8),
+                "Посылка должна быть свежей в 8-й день (день окончания срока)");
+    }
 
-        // Граничный сценарий последний день срока
-        assertFalse(pp.isExpired(8), "Должна быть годна в последний день срока (8)");
-
-        // Просрочк
-        assertTrue(pp.isExpired(9), "Должна быть испорчена на 9-й день");
+    @Test
+    void shouldBeExpiredAfterDeadline() {
+        assertTrue(parcel.isExpired(9),
+                "Посылка должна считаться испорченной на 9-й день");
     }
 }
