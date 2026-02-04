d53ca79ec7ace9aed87b251b5bca5af48fb96d62
kirusha <kirillkomlyashov@yandex.ru>
2026-02-04 14:04:27 +0300

    add delivery cost tests for all parcel types and zero weight

--- test/ru/yandex/practicum/DeliveryCostTest.java
+++ test/ru/yandex/practicum/DeliveryCostTest.java
@@ -2,18 +2,48 @@ package ru.yandex.practicum;
 
 import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.delivery.FragileParcel;
+import ru.yandex.practicum.delivery.PerishableParcel;
 import ru.yandex.practicum.delivery.StandardParcel;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DeliveryCostTest {
+
     @Test
-    void shouldCalculateCorrectCost() {
-        StandardParcel standard = new StandardParcel(10, 1, "Книги", "Москва");
-        FragileParcel fragile = new FragileParcel(5, 1, "Ваза", "Питер");
+    void shouldCalculateCostForStandardParcel() {
+        StandardParcel standard = new StandardParcel(10, 1, "Книги", "Таганрог");
 
-        assertEquals(20, standard.calculateDeliveryCost(), "Стандартная посылка считается неверно");
+        int actualCost = standard.calculateDeliveryCost();
 
-        assertEquals(20, fragile.calculateDeliveryCost(), "Хрупкая посылка считается неверно");
+        assertEquals(20, actualCost, "Стоимость стандартной посылки считается неверно");
+    }
+
+    @Test
+    void shouldCalculateCostForFragileParcel() {
+        FragileParcel fragile = new FragileParcel(5, 1, "Реликвия", "Ростов");
+
+        int actualCost = fragile.calculateDeliveryCost();
+
+        assertEquals(20, actualCost, "Стоимость хрупкой посылки считается неверно");
+    }
+
+    @Test
+    void shouldCalculateCostForPerishableParcel() {
+        PerishableParcel perishable = new PerishableParcel(10, 1, "Молоко",
+                "Осташков", 3);
+
+        int actualCost = perishable.calculateDeliveryCost();
+
+        assertEquals(30, actualCost, "Стоимость скоропортящейся посылки считается неверно");
+    }
+
+    @Test
+    void shouldCalculateZeroCostForZeroWeight() {
+        StandardParcel zeroWeightParcel = new StandardParcel(0, 1,
+                "Воздух", "Небо");
+
+        int actualCost = zeroWeightParcel.calculateDeliveryCost();
+
+        assertEquals(0, actualCost, "Посылка с нулевым весом должна стоить 0");
     }
 }
